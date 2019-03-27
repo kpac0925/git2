3cc0d38821406d25bc2cab549afa9f302ec09b51
kpac <kongpac@naver.com>
2019-03-27 11:40:36 +0900

    dd

--- hi/src/hi/hi.java
+++ hi/src/hi/hi.java
@@ -7,7 +7,7 @@ System.out.println("heedddo World");
 
 System.out.println("hello World");
 
-System.out.println("hellsssssso World");
+System.out.println("hellssssfffffsso World nanioitteirukazenzenwakarimaseng");
 
 
 
